382f249af9f877a3b951ab5426e836b50f7a0634
Gonzalo Daniel Aguilar <aguilar.daniel.gonzalo@gmail.com>
2022-11-11 19:06:19 -0300

    Var Mut

--- platzi-curso/src/main.rs
+++ platzi-curso/src/main.rs
@@ -1,6 +1,8 @@
 fn main() {
     let nombre: &str = "Gonzalo";
-    let edad: u8 = 34;
+    let mut edad: u8 = 33;
+
+    edad = edad + 1;
 
     println!("Hola soy {} y tengo {} años", nombre, edad);
 }
